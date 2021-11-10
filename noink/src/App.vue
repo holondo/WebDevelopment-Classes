9d6615799a644eeccbefa55f101848401c4de495
Guilherme Holanda Sanches <38058605+holondo@users.noreply.github.com>
2021-11-09 23:31:49 -0300

    Homepage wireframe created

--- noink/src/App.vue
+++ noink/src/App.vue
@@ -5,10 +5,8 @@
       color="white"
       flat
     >
-      <v-avatar
-        :color="$vuetify.breakpoint.smAndDown ? 'grey darken-1' : 'transparent'"
-        size="32"
-      ></v-avatar>
+      <v-app-bar-title>NoInK</v-app-bar-title>
+      
 
       <v-tabs
         centered
@@ -17,67 +15,34 @@
       >
         <v-tab
           v-for="link in links"
-          :key="link"
+          :key="link" :to="link[1]"
         >
-          {{ link }}
+          <router-link :to="link[1]" style="text-decoration: none; color: inherit;">{{ link[0]}}</router-link>
+          
         </v-tab>
       </v-tabs>
 
       <v-text-field
             v-model="message2"
-            solo
-            label="Solo"
+            outlined
+            label="Search"
             rounded
             clearable
             append-icon="mdi-magnify"
+            class="my-auto mr-3"
       ></v-text-field>
-      
-      <v-avatar
-        class="hidden-sm-and-down"
-        color="grey darken-1 shrink"
-        size="32"
-      ></v-avatar>
+
+      <v-btn icon  to="profile">
+        <v-avatar
+          color="grey darken-1 shrink"
+          size="32"
+        ></v-avatar>
+      </v-btn>
     </v-app-bar>
 
     <v-main class="grey lighten-3">
       <v-container>
-        <v-row>
-          <v-col
-            cols="12"
-            sm="2"
-          >
-            <v-sheet
-              rounded="lg"
-              min-height="268"
-            >
-              <!--  -->
-            </v-sheet>
-          </v-col>
-
-          <v-col
-            cols="12"
-            sm="8"
-          >
-            <v-sheet
-              min-height="70vh"
-              rounded="lg"
-            >
-              <!--  -->
-            </v-sheet>
-          </v-col>
-
-          <v-col
-            cols="12"
-            sm="2"
-          >
-            <v-sheet
-              rounded="lg"
-              min-height="268"
-            >
-              <!--  -->
-            </v-sheet>
-          </v-col>
-        </v-row>
+        <router-view></router-view>
       </v-container>
     </v-main>
   </v-app>
@@ -87,10 +52,8 @@
   export default {
     data: () => ({
       links: [
-        'Dashboard',
-        'Messages',
-        'Profile',
-        'Updates',
+        ['Home', '/', ],
+        ['Explore', '/about']
       ],
     }),
   }
--- noink/src/views/Home.vue
+++ noink/src/views/Home.vue
@@ -1,15 +1,88 @@
 <template>
-  <hello-world />
+  <v-container>
+    <!-- Read recently -->
+    <v-row>
+      <v-col cols="12">
+        <v-card >
+          <v-card-title >Read Recently</v-card-title>
+          <v-row class="px-3">
+            <v-col
+              v-for="i in 6"
+              :key="i"
+              cols="2"
+              md="2"
+            >
+              <router-link style="text-decoration: none; color: inherit" to="produto">
+                <div>
+                  <v-sheet color="warning" height="150" ></v-sheet>
+                  <h4>Producto</h4><br>
+                  Descricao
+                </div>
+              </router-link>
+            </v-col>
+          </v-row>
+        </v-card>
+      </v-col>
+    </v-row>
+
+    <v-row>
+      <v-col cols="12">
+        <v-sheet color="info" rounded>
+          <v-text h1>Recommended for you</v-text>
+        </v-sheet>
+      </v-col>
+    </v-row>
+
+    <!-- Products -->
+    <v-row  v-for="n in 4" :key="n">
+      <v-col cols="12">
+        <v-card >
+          <v-card-title >Category {{n}}</v-card-title>
+          <v-row class="px-3">
+            <v-col
+              v-for="j in 6"
+              :key="`${n}${j}`"
+              cols="2"
+              md="2"
+            >
+              <router-link style="text-decoration: none; color: inherit" to="produto">
+                <div>
+                  <v-sheet color="warning" height="150" ></v-sheet>
+                  <h4>Producto</h4><br>
+                  Descricao
+                </div>
+              </router-link>
+            </v-col>
+          </v-row>
+        </v-card>
+      </v-col>
+    </v-row>
+    <!-- <v-row>
+      <template v-for="n in 4">
+        <v-col
+          :key="n"
+          class="mt-2"
+          cols="12"
+        >
+          <h3 class="text-center">Category {{ n }}</h3>
+        </v-col>
+
+        <v-col
+          v-for="j in 6"
+          :key="`${n}${j}`"
+          cols="6"
+          md="2"
+        >
+          <v-sheet height="150"></v-sheet>
+        </v-col>
+      </template>
+    </v-row> -->
+  </v-container>
 </template>
 
 <script>
-  import HelloWorld from '../components/HelloWorld'
 
   export default {
     name: 'Home',
-
-    components: {
-      HelloWorld,
-    },
   }
 </script>
