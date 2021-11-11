6f578cc7cf5f21d363e61ba18c94eb46d7168a84
Guilherme Holanda Sanches <38058605+holondo@users.noreply.github.com>
2021-11-11 16:20:02 -0300

    Main app modularized

--- noink/src/App.vue
+++ noink/src/App.vue
@@ -1,45 +1,6 @@
 <template>
   <v-app id="inspire">
-    <v-app-bar
-      app
-      color="white"
-      flat
-    >
-      <v-app-bar-title>NoInK</v-app-bar-title>
-      
-
-      <v-tabs
-        centered
-        class="ml-n9"
-        color="grey darken-1"
-      >
-        <v-tab
-          v-for="link in links"
-          :key="link" :to="link[1]"
-        >
-          <router-link :to="link[1]" style="text-decoration: none; color: inherit;">{{ link[0]}}</router-link>
-          
-        </v-tab>
-      </v-tabs>
-
-      <v-text-field
-            v-model="message2"
-            outlined
-            label="Search"
-            rounded
-            clearable
-            append-icon="mdi-magnify"
-            class="my-auto mr-3"
-      ></v-text-field>
-
-      <v-btn icon  to="profile">
-        <v-avatar
-          color="grey darken-1 shrink"
-          size="32"
-        ></v-avatar>
-      </v-btn>
-    </v-app-bar>
-
+    <top-bar/>
     <v-main class="grey lighten-3">
       <v-container>
         <router-view></router-view>
@@ -49,12 +10,16 @@
 </template>
 
 <script>
+  import TopBar from '.\\components\\TopBar.vue'
   export default {
+    name : "App",
+    components : {
+      TopBar
+    },
     data: () => ({
-      links: [
-        ['Home', '/', ],
-        ['Explore', '/about']
-      ],
+      user : null
     }),
+    created(){
+    }
   }
 </script>
