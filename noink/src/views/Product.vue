867beef94deae537c95f89e72fdfa457530a2748
Guilherme Holanda Sanches <38058605+holondo@users.noreply.github.com>
2021-11-17 19:59:06 -0300

    Add to wishlist feature added

--- noink/src/views/Product.vue
+++ noink/src/views/Product.vue
@@ -5,8 +5,9 @@
             <v-col cols="12" >
                     <v-card-title>{{product.name}}</v-card-title>
                     <v-img :src="product.img_src" width="15vw" class="mx-auto"> </v-img>
-                    <h3 class="mx-auto">Price: ${{product.price}}</h3>
+                    <h3 class="text-center">Price: ${{product.price}}</h3>
             </v-col>
+                    <v-btn color="primary" class="mx-auto" v-if="$store.getters.isLogged" @click.prevent="addToWishlist">Add to Wishlist</v-btn>
         </v-row>
                 </v-card>
     </v-container>
@@ -24,6 +25,11 @@
             if(productID <= this.$store.state.products.length){
                 this.product = this.$store.state.products[productID]
             }
-        }
+        },
+        methods: {
+            addToWishlist(){
+                this.$store.dispatch("addToWishlist", this.product.id)
+            }
+        },
     } 
 </script>
